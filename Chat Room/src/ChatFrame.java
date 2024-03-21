377e813f68ee185287ddcc8d4dd5f4d9005e1193
nischalkonduri <94252271+nischalkonduri@users.noreply.github.com>
2024-03-20 21:45:05 -0500

    exiting, entering, frame
    this sets the format for all the format and also the exit

--- Chat Room/src/ChatFrame.java
+++ Chat Room/src/ChatFrame.java
@@ -6,4 +6,65 @@ import java.io.ObjectOutputStream;
 
 public class ChatFrame extends JFrame implements ActionListener {
 
+    private JTextArea chatArea;
+    private JTextField messageField;
+    private JButton sendButton;
+    private JButton exitButton;
+    private JList<String> userList;
+    private DefaultListModel<String> userListModel;
+    private String username;
+    private ObjectOutputStream outputStream;
+
+    public ChatFrame(String username) {
+        super("Chat - " + username);
+        this.username = username;
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        setSize(600, 400);
+        setLayout(new BorderLayout());
+
+
+        chatArea = new JTextArea();
+        chatArea.setEditable(false);
+        JScrollPane chatScrollPane = new JScrollPane(chatArea);
+        add(chatScrollPane, BorderLayout.CENTER);
+
+
+        JPanel bottomPanel = new JPanel(new BorderLayout());
+        messageField = new JTextField();
+        bottomPanel.add(messageField, BorderLayout.CENTER);
+
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        sendButton = new JButton("Send");
+        sendButton.addActionListener(this);
+        buttonPanel.add(sendButton);
+
+
+        exitButton = new JButton("Exit");
+        exitButton.addActionListener(this);
+        buttonPanel.add(exitButton);
+
+
+        bottomPanel.add(buttonPanel, BorderLayout.SOUTH);
+        add(bottomPanel, BorderLayout.SOUTH);
+
+
+        userListModel = new DefaultListModel<>();
+        userList = new JList<>(userListModel);
+        JScrollPane userScrollPane = new JScrollPane(userList);
+        userScrollPane.setPreferredSize(new Dimension(150, getHeight()));
+        add(userScrollPane, BorderLayout.EAST);
+
+
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                exitChat();
+            }
+        });
+
+
+        setVisible(true);
+    }
+
+
 }
--- Chat Room/src/ClientListener.java
+++ Chat Room/src/ClientListener.java
@@ -1,18 +1,17 @@
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ClientListener implements Runnable
 {
-    private ObjectInputStream is = null;
-    private ObjectOutputStream os = null;
-    private ChatFrame frame = null;
+    private ObjectInputStream inputStream;
+    private ChatFrame chatFrame;
 
-    public ClientListener(ObjectInputStream is,
-                          ObjectOutputStream os,
-                          ChatFrame frame) {
-        this.is = is;
-        this.os = os;
-        this.frame = frame;
+    public ClientListener(ObjectInputStream inputStream,
+                          ChatFrame chatFrame) {
+        this.inputStream = inputStream;
+        this.chatFrame = chatFrame;
 
     }
 
@@ -22,68 +21,13 @@ public class ClientListener implements Runnable
         {
             while(true)
             {
-                MessageToClient cfs = (MessageToClient)is.readObject();
-
-                if(cfs.getCommand()== MessageToClient.CLOSING) {
-                    try {
-                        frame.closing();
-                    } catch (InterruptedException e) {
-                        System.out.println("Exception exit");
-                    }
-                }
-                if(cfs.getCommand()== MessageToClient.CONFIRMRESET) {
-                    System.out.println("Confirming repaint from clientlistener");
-                    frame.resetGrid();
-                    frame.setTurn('X');
-                    frame.repaint();
-                }
-                // processes the received command
-                if(cfs.getCommand()== MessageToClient.RESET) {
-                    //frame.resetGrid();
-                    //frame.setTurn('X');
-                    if(!frame.getSentResetRequest()) {
-                        frame.initializeConfirm("Your opposition would like to rematch Press OK to accept a rematch, else exit out the pop up", "Rematch Request");
-
-                    }
-                    //display for only one of the frames
-                    frame.repaint();
-                    //System.out.println(frame.getGrid());
-                }
-                else if(cfs.getCommand() == MessageToClient.X_TURN) {
-                    frame.setTurn('X');
-                    //frame.repaint();
-                    //System.out.println(frame.getGrid());
-                }
-                else if(cfs.getCommand() == MessageToClient.O_TURN)
-                    frame.setTurn('O');
-                else if(cfs.getCommand() == cfs.MOVE)
-                {
-                    String data = cfs.getData();
-                    // pulls data for the move from the data field
-                    int c = data.charAt(0) - '0';
-                    int r = data.charAt(1) - '0';
-
-
-                    // changes the board and redraw the screen
-                    frame.makeMove(c,r,data.charAt(2));
-                }
-                // handles the various end game states
-                else if(cfs.getCommand() == MessageToClient.TIE)
-                {
-                    frame.setText("Tie game.");
-                    frame.setTurn('e');
-                }
-                else if(cfs.getCommand() == MessageToClient.X_WINS)
-                {
-                    frame.setText("Red wins!");
-                    frame.setTurn('e');
-
-                }
-                else if(cfs.getCommand() == MessageToClient.O_WINS)
-                {
-                    frame.setText("Black wins!");
-                    frame.setTurn('e');
-
+                MessageToClient message = (MessageToClient) inputStream.readObject();
+                if (message.getMessage().equals("exit")) {
+                    //chatFrame.appendMessage(message.getSender() + " disconnected");
+                    //chatFrame.removeUser(message.getSender());
+                } else {
+                    //chatFrame.appendMessage(message.getSender() + ": " + message.getMessage());
+                    //chatFrame.updateUserList(message.getUserList()); // Update user list in UI
                 }
             }
         }
