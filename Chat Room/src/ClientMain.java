3446086e5b36c259ef57b63da7957ab722ee83a2
nischalkonduri <94252271+nischalkonduri@users.noreply.github.com>
2024-03-21 09:30:36 -0500

    in the middle, will be done by period end
    changed from JOptionPane to in console

--- Chat Room/src/ClientMain.java
+++ Chat Room/src/ClientMain.java
@@ -2,20 +2,24 @@ import javax.swing.*;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class ClientMain {
+    private static ObjectOutputStream outputStream;
     public static void main(String[] args) {
         try {
 
             String username;
             boolean validName = false;
+            Scanner scanner = new Scanner(System.in);
             do {
-                username = JOptionPane.showInputDialog("Enter username: ");
+                System.out.print("Enter your Username: ");
+                username = scanner.nextLine();
                 if (username != null && !username.trim().isEmpty()) {
                     validName = true;
                 }
                 else {
-                    JOptionPane.showMessageDialog(null, "Pleae enter a valid username");
+                    System.out.println("Please enter a valid username: ")
                 }
             }
             while (!validName);
@@ -23,30 +27,22 @@ public class ClientMain {
 
             // create a connection to server
             Socket socket = new Socket("127.0.0.1", 8011);
-            ObjectInputStream is = new ObjectInputStream(socket.getInputStream());
-            ObjectOutputStream os = new ObjectOutputStream(socket.getOutputStream());
+            outputStream = new ObjectOutputStream(socket.getOutputStream());
 
 
             // Determine if playing as X or O
             MessageToClient cfs = (MessageToClient) is.readObject();
             ChatFrame frame;
 
-
-            // Create the Frame based on which player the server says this client is
-            /*if (cfs.getCommand() == CommandFromServer.CONNECTED_AS_X) {
-                frame = new Connect4Frame(chatData, os, 'X');
-                frame.setText("Waiting for Black to connect"); // Set initial text
-            } else {
-                frame = new Connect4Frame(chatData, os, 'O');
-            }*/
-
-
-            // Starts a thread that listens for commands from the server
-            ClientListener cl = new ClientListener(is, os, frame);
+            ClientListener cl = new ClientListener(is, os);
             Thread t = new Thread(cl);
             t.start();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+    public static ObjectOutputStream getOutputStream() {
+        return outputStream;
+    }
+
 }
